25569b761345d2f6f86b635dcb34686cde320912
devinqtran <devinqtran@gmail.com>
2026-08-18 10:43:19 -0600

    practice with expressions and parsing

---
+++ practice/expr.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+// std::string get_token() {
+//     return curr_token;
+// }
+
+// expr handles addition and subtraction
+// double expr() {
+//     double left = term;
+
+//     for(;;) // forever
+//         switch(curr_tok) {
+//             case PLUS:
+//                 get_token(); // eat '+'
+//                 left += term();
+//                 break;
+//             case MINUS:
+//                 get_token(); // eat '-'
+//                 left -= term();
+//                 break;
+//             default:
+//                 return left;
+//             }
+// }
