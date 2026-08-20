ee49a44aa0d28aca7abc81cad5999dfb5d0f0fa8
devinqtran <devinqtran@gmail.com>
2026-08-20 09:22:09 -0600

    Implemented successful try...catch loop in parse function to throw the error token if it does not follow the grammar.

--- school/cs236/Project2/Parser.h
+++ school/cs236/Project2/Parser.h
@@ -3,55 +3,88 @@
 
 #include "..//Project1//Token.h"
 
-class Parser {
+class Parser
+{
 private:
     vector<Token> tokens; // a private vector of Token objects called token
 
 public:
     // Constructor receives a reference to the tokens vector of Token objects
-    Parser(const vector<Token>& tokens) : tokens(tokens) {}
+    Parser(const vector<Token> &tokens) : tokens(tokens) {}
+
+    // Main parse function try...catch loop 
+    void parse() {
+        try {
+            // TESTING REMOVE AFTER datalogProgram is finished
+            scheme();
+
+            // parse top-level grammar rule
+            // datalogProgram(); // Need to implement
+
+            // print success if it worked without exceptions
+            std::cout << "Success!" << "\n";
+        }    
+        catch (const Token& errorToken) {
+            // catch the thrown error token and report error
+            std::cout << "Failure!" << "\n";
+            std::cout << "  " << errorToken.toString() << "\n";
+        }
+    }
 
     // tokenType function returns the type of the current Token
-    TokenType tokenType() const {
+    TokenType tokenType() const
+    {
         return tokens.at(0).getType();
     }
 
     // advanceToken function moves to the next Token
-    void advanceToken() {
-        tokens.erase(tokens.begin()); 
+    void advanceToken()
+    {
+        tokens.erase(tokens.begin());
     }
-    
+
     // throwError function (INCOMPLETE)
-    void throwError() {
-        std::cout << "error" << std::endl;
+    void throwError()
+    {
+        // Throw the current token as an error
+        throw tokens.at(0);
     }
 
     // match function called when parsing a terminal symbol
-    void match(TokenType t) {
+    void match(TokenType t)
+    {
         cout << "match: " << t << endl;
 
-        if (tokenType() == t) {
+        if (tokenType() == t)
+        {
             advanceToken();
         }
-        else {
+        else
+        {
             throwError();
         }
     }
 
     // idList -> COMMA ID idList | lambda
-    void idList() {
-        if (tokenType() == COMMA) {
+    void idList()
+    {
+        if (tokenType() == COMMA)
+        {
             match(COMMA);
             match(ID);
             idList();
-        } else {
+        }
+        else
+        {
             // lambda
         }
     }
 
     // scheme -> ID LEFT_PAREN ID idList RIGHT_PAREN
-    void scheme() {
-        if (tokenType() == ID) {
+    void scheme()
+    {
+        if (tokenType() == ID)
+        {
             match(ID);
             match(LEFT_PAREN);
             match(ID);
@@ -60,4 +93,14 @@ public:
         }
     }
 
+    /*
+        Remaining Tasks to finish Datalog Parser:
+        1. Write parsing functions for the remaining grammar rules. (16 total)
+
+        2. Fix error handling in the parser. (Throw an Exception in the 'throwError' function.) (Catch the Exception at the top of the parser and report the error.)
+
+        3. Write classes for Parameter, Predicate, Rule, and Datalog Program. (4 classes containing rules from Parser.h grammars)
+
+        4. Add code to the parser to create Parameter, Predicate, and Rule objects while parsing, and construct a Datalog Program object that contains lists of Schemes, Facts, Rules, and Queries.
+    */
 };
--- school/cs236/Project2/main.cpp
+++ school/cs236/Project2/main.cpp
@@ -8,14 +8,16 @@ int main() {
 
   vector<Token> tokens = {
     Token(ID,"Ned",2),
-    Token(LEFT_PAREN,"(",2),
+    // Token(LEFT_PAREN,"(",2),
     Token(ID,"Ted",2),
     Token(COMMA,",",2),
     Token(ID,"Zed",2),
     Token(RIGHT_PAREN,")",2),
   };
 
-  Parser p = Parser(tokens);
-  p.scheme();
+  Parser parser(tokens);
+  parser.parse();
+
+  return 0;
 
 }
