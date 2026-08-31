9b3e9de786a61e7a6228fbe89133e0d437132420
devinqtran <devinqtran@gmail.com>
2026-08-31 09:13:18 -0600

    Added helper methods to Node.h class.

--- school/cs236/Lab5/Node.h
+++ school/cs236/Lab5/Node.h
@@ -8,13 +8,34 @@ using namespace std;
 class Node {
 private:
     set<int> adjacentNodeIDs;
+    bool visited;
 
 public:
+
+    // Constructor
+    Node() {
+        visited = false;
+    }
     
     void addEdge(int adjacentNodeID) {
         adjacentNodeIDs.insert(adjacentNodeID);
     }
 
+    // method for marking if it has been visited
+    void markVisited() {
+        visited = true;
+    }
+
+    // method for checking if it was visited
+    bool isVisited() const {
+        return visited;
+    }
+
+    // method for getting the adjacency list
+    const set<int>& getAdjacentNodes() const {
+        return adjacentNodeIDs;
+    }
+    
     // The 'toString' function prints the node IDs in the 'adjacent node IDs' set as a comma separated list. 
     // The letter R decoration is printed before each node ID. The node IDs are printed in sorted order. (Using a 'set' makes the sorting automatic.)
     string toString() const {
